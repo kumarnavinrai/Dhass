7abc8ff7bb6841bc60b059de75f24b23a394715f
Navin Kumar <kumarnavinrai@hotmail.com>
2023-07-14 21:37:22 +0800

    second commit

---
+++ app/src/main/java/com/example/dhass/PlaceholderFragment.java
@@ -0,0 +1,25 @@
+package com.example.dhass;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PlaceholderFragment extends Fragment {
+
+    public PlaceholderFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_placeholder, container, false);
+        // Customize the fragment's layout and functionality here
+        return view;
+    }
+}
